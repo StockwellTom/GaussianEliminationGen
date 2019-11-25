cc7e231389d427df57a143160487334b6bef046d
StockwellTom <stockwellrtom@gmail.com>
2019-11-25 13:10:00 +0000

    Added echelon form solver
    Doesn't always work in specific situations such as when there's no solutions, and it's left in a weird decimal form. Apart from that it's okay I think!
    If you wanna solve in more dimensions that what the ASCII converter thing would allow just comment out that bit of code, it only writes the variables into a classic linear equation format.

--- gaussianElimanationCreator.py
+++ gaussianElimanationCreator.py
@@ -8,13 +8,13 @@ equatStrings = []
 
 #randomly choosing the values of the dimensions, e.g x,y,z in three dimensions
 for i in range(dimensions):
-    variables.append(random.randint(-5, 5)) #these are the bounds in which your variables to solve can occupy
+    variables.append(random.randint(1, 5)) #these are the bounds in which your variables to solve can occupy
 
 #randomly choosing the coefficients to generate the equations
 for i in range(dimensions):
     coefficients.append([])
     for j in range(dimensions):
-        coefficients[i].append(random.randint(-3, 3)) #these are the bounds in which your coefficients to solve can occupy
+        coefficients[i].append(random.randint(-5, 5)) #these are the bounds in which your coefficients to solve can occupy
 
 #creating a string format for the equatuons for the user
 for i in range(dimensions):
@@ -39,9 +39,31 @@ for i in range (len(equatStrings)):
     equatStrings[i] = equatStrings[i] + str(totals[i])
 
 print(equatStrings)
-
+print(coefficients)
 input("press enter to see answers")
 
 for i in range(len(variables)):
     print (chr(123 - dimensions + i) + " = " + str(variables[i]))
 
+input("press enter to see solution")
+
+matrix = []
+
+#generating matrix format
+for i in range(dimensions):
+    matrix.append([])
+    for j in range(dimensions):
+        matrix[i].append(coefficients[i][j])
+    matrix[i].append(totals[i])
+
+print(matrix)
+
+
+for pivots in range(dimensions):
+    row = dimensions - 1
+    while row > pivots:
+        pivotMult = matrix[row][pivots] / matrix[pivots][pivots]
+        for values in range(dimensions + 1):
+            matrix[row][values] = matrix[row][values] - pivotMult*(matrix[pivots][values])
+        print(matrix)
+        row = row - 1
